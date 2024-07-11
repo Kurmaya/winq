053c8fccd387cf46507c8d90134a1ea693ad92e7
Kurmaya Siddhartha <kurmaya.sid@gmail.com>
2024-07-12 03:01:34 +0530

    animation updates + products section update

--- scripts/homePageScript.js
+++ scripts/homePageScript.js
@@ -55,6 +55,7 @@ let whyAnim = gsap.timeline({
     scrollTrigger:{
         trigger:'#hero',
         start:'-50px 1%',
+        end: 'bottom 10%',
         scrub:true,
         // markers:true,
         toggleActions: 'play none reverse none',
@@ -72,7 +73,7 @@ whyAnim.to(circle,{
     x:'80%',  
 })
 whyAnim.to(circle,{
-    top:(rightRect.getBoundingClientRect().top+window.scrollY)-rightRect.getBoundingClientRect().height/2,
+    top:(rightRect.getBoundingClientRect().top-8+window.scrollY)-rightRect.getBoundingClientRect().height/2,
     x:'50%',  
     duration:.5,
 })
@@ -81,22 +82,23 @@ whyAnim.to(rightRect.querySelectorAll('img')[0],{
     rotateZ:'-45deg',
     x:'10%',
     y:20,
-    duration:.2
+    duration:.3
 },'-=0.1')
 whyAnim.to(rightRect.querySelectorAll('img')[1],{
     rotateZ:'-45deg',
     x:'10%',
     y:20,
-    duration:.2
+    duration:.3
 },'-=0.1')
 
 const leftt = leftRect.getBoundingClientRect().top;
 
 whyAnim.to(circle,{
-    top:(leftt+window.scrollY)-leftRect.getBoundingClientRect().height/2,
-    x:'-650%',  
+    top:12+(leftt+window.scrollY)-leftRect.getBoundingClientRect().height/2,
+    // x:'-660%',  
+    x: leftRect.getBoundingClientRect().right-window.innerWidth,
     duration:.8,    
-},'-=0.1')
+},'-=0.4')
 whyAnim.to(leftRect,{
     rotateZ:'200deg',
     duration:.4,
@@ -109,10 +111,11 @@ whyAnim.to(circle,{
 },'-=0.3')
 
 whyAnim.to(circle,{
-    top:((usSpan.top)-15-usSpan.height/2)+window.scrollY,
-    x:-((usSpan.left/2)+5)-usSpan.width/1.5,
+    top:((usSpan.top)-15-usSpan.height/1.5)+window.scrollY,
+    // x:-((usSpan.left/2)+7)-usSpan.width/1.5,
+    x:usSpan.right-window.innerWidth+4*(usSpan.width),
     scale:.2,
-    opacity:0,  
+    // opacity:0,  
     duration:.5,
 })
 whyAnim.to('.section-holder',{
@@ -123,9 +126,9 @@ whyAnim.to('.section-holder',{
         y:200,
         ease:"bounce.out"
     },'<')
-    whyAnim.to(spn,{
-        opacity:1,
-    },'-=0.1')
+    // whyAnim.to(spn,{
+    //     opacity:1,
+    // },'-=0.1')
 
 
 //section 2-3 animation
@@ -139,42 +142,438 @@ let prodsAnim = gsap.timeline({
         toggleActions: 'play none reverse none',
     }
 })
+prodsAnim.to(spn,{
+    opacity:0,
+})
 prodsAnim.to(circle,{
-    scale:.2,
+    // scale:.2,
     opacity:1,
-    duration:.4,
-})
-
-prodsAnim.to(circle,{
-    y:(offset-(circle.getBoundingClientRect().top-bodyRect.top))-32,
+    // duration:.4,
+    y:(document.querySelectorAll('.p-holder p')[2].getBoundingClientRect().top)/2,
     x:'+=12vw',
-    duration:1,
-})
-
-prodsAnim.to('#us > img',{
-    y:1000,
-  })
-
-prodsAnim.to('.products-holder',{
-    bottom:'-10%',
+    duration:.5,
+    scale:.4,
 },'<')
-
-
-prodsAnim.to('.products-holder > .text-holder',{
+prodsAnim.to(circle,{
+    // scale:.2,
+    opacity:1,
+    // duration:.4,
+    // y:((offset-((circle.getBoundingClientRect().top)))/1.8)+20,
+    y:(aiLayer.getBoundingClientRect().y/2)+(aiLayer.getBoundingClientRect().height),
+    duration:1,
+},'+=0.5')
+prodsAnim.to(circle,{
+    scale:1,
+})
+prodsAnim.to(circle,{
+    width:300,
+    // height:document.querySelector('.exp').style.height,
+    borderRadius:'25px',
+    x:(window.innerWidth/2)-aiLayer.getBoundingClientRect().right,
+    zIndex:'9!important',
+    opacity:0,
+})
+prodsAnim.to('.products-holder .text-holder ',{
     opacity:1,
     
+},'<')
+prodsAnim.to('.prods-bg',{
+    rotateZ:'-110deg',
+    borderRadius: '25%',
 })
+prodsAnim.to('#us > img',{
+    y:400,
+  },'<')
+
+
+// prodsAnim.to(circle,{
+//     x:'-450%',
+//     duration:.5,
+// })
 prodsAnim.to('.exp',{
     background:'#EA5583',
     borderRadius:'25px',
-    left:'-5px',
+    // left:'-5px',
     paddingLeft:'1rem',
     
-})
-prodsAnim.to(circle,{
-    opacity:0,
-    duration:.1,
 },'<')
-prodsAnim.to('.products-holder > .image-holder',{
+// prodsAnim.to(circle,{
+//     opacity:0,
+//     duration:.1,
+// },'<')
+prodsAnim.to('.products-holder .image-holder',{
     left:0    
 })
+
+const prodsList = document.querySelectorAll('.prods-list li');
+const prodsHead = document.getElementById('prod-sub');
+const prodsPara = document.getElementById('prod-p');
+
+    prodsList.forEach(p=> {
+        p.addEventListener('click',(e)=>{
+            prodsList.forEach(ac=>{
+                ac.classList.remove('active');
+            })
+            let tl = gsap.timeline();
+            switch (e.target.textContent) {
+                case '1':
+                    
+                    tl.to(prodsHead,{
+                        textContent:' ',
+                        width:0,
+                        opacity:0,
+                        duration:.3,
+                        ease:'power1'
+                    })
+                    .to(prodsPara,{
+                        textContent:' ',
+                        opacity:0,
+                        duration:0.3
+                    },'<')
+                    .to('.image-holder',{
+                        left:'-100%',
+                        duration:0.2,
+                        ease:'power1'
+                    },'<')
+        
+                    .to(prodsHead,{
+                        opacity:1,
+                    })
+                    
+                    .to(prodsHead,{
+                        width:'auto',
+                        // duration:.3,
+                        // ease:'power1'
+                    })
+                    .to(prodsHead,{
+                        textContent:'AI-Layered Marketing:',
+                    })
+                    .to(prodsPara,{
+                        textContent:'WINQ empowers brokers with a powerful AI marketing suite for cross-platform posting, lead generation and complete social media management.',
+                        opacity:1,
+                    },'<')
+                    .set('.image-holder>img',{attr:{src:"./assets/bot.png"}})
+                    .to('.image-holder',{
+                        left:'0',
+                        duration:0.2,
+                        ease:'power1'
+                    },'<')
+                  
+                    console.log('1');
+                    e.target.classList.add('active');
+                    break;
+                case '2':
+                    
+                    tl.to(prodsHead,{
+                        textContent:' ',
+                        width:0,
+                        opacity:0,
+                        duration:.1,
+                        ease:'power1'
+                    })
+                    .to(prodsPara,{
+                        textContent:' ',
+                        opacity:0,
+                        duration:0.1
+                    },'<')
+                    .to('.image-holder',{
+                        left:'-150%',
+                        duration:0.2,
+                        ease:'power1'
+                    },'<')
+                    .to(prodsHead,{
+                        opacity:1,
+                    })
+                    
+                    .to(prodsHead,{
+                        width:'auto',
+                        // duration:.3,
+                        // ease:'power1'
+                    })
+                    .to(prodsHead,{
+                        textContent:'Digital Insurance Storefront with unlimited social & marketing tools:',
+                    })
+                    .to(prodsPara,{
+                        textContent:'WINQ empowers brokers with a diverse portfolio of insurance products and the ability to create unlimited social media content, landing pages, and product-specific campaigns designed to enhance their social/digital presence and build brand awareness. Our social media experts provide personalised guidance, helping you craft a captivating online strategy that resonates with your audience and strengthens your brand.',
+                        opacity:1,
+                    },'<')
+                    .set('.image-holder>img',{attr:{src:"./assets/prods-hand.png"}})
+                    .to('.image-holder',{
+                        left:'0',
+                        duration:0.2,
+                        ease:'power1'
+                    },'<')
+                    console.log('2');
+                    e.target.classList.add('active');
+                    break;
+                    case '3':
+                    
+                    tl.to(prodsHead,{
+                        textContent:' ',
+                        width:0,
+                        opacity:0,
+                        duration:.1,
+                        ease:'power1'
+                    })
+                    .to(prodsPara,{
+                        textContent:' ',
+                        opacity:0,
+                        duration:0.1
+                    },'<')
+                    .to('.image-holder',{
+                        left:'-150%',
+                        duration:0.2,
+                        ease:'power1'
+                    },'<')
+                    .to(prodsHead,{
+                        opacity:1,
+                    })
+                    
+                    .to(prodsHead,{
+                        width:'auto',
+                        // duration:.3,
+                        // ease:'power1'
+                    })
+                    .to(prodsHead,{
+                        textContent:'Intuitive Quote Management:',
+                    })
+                    .to(prodsPara,{
+                        textContent:"WINQ's intuitive quote management system simplifies customer effort by relating to the buyer's specific needs and then comparing and presenting quotes from various insurance carriers, saving valuable time and effort. No more cookie-cutter quotes!",
+                        opacity:1,
+                    },'<')
+                    .set('.image-holder>img',{attr:{src:"./assets/prods-hand.png"}})
+                    .to('.image-holder',{
+                        left:'0',
+                        duration:0.2,
+                        ease:'power1'
+                    },'<')
+                    console.log('3');
+                    e.target.classList.add('active');
+                    break;
+                    case '4':
+                    
+                    tl.to(prodsHead,{
+                        textContent:' ',
+                        width:0,
+                        opacity:0,
+                        duration:.1,
+                        ease:'power1'
+                    })
+                    .to(prodsPara,{
+                        textContent:' ',
+                        opacity:0,
+                        duration:0.1
+                    },'<')
+                    .to('.image-holder',{
+                        left:'-150%',
+                        duration:0.2,
+                        ease:'power1'
+                    },'<')
+                    .to(prodsHead,{
+                        opacity:1,
+                    })
+                    
+                    .to(prodsHead,{
+                        width:'auto',
+                        // duration:.3,
+                        // ease:'power1'
+                    })
+                    .to(prodsHead,{
+                        textContent:'Multi-Carrier Marketplace: ',
+                    })
+                    .to(prodsPara,{
+                        textContent:'The WINQ marketplace enables brokers to present a diverse array of products from leading insurance companies, catering to a wide spectrum of client needs and maximizing sales opportunities. This curated platform empowers brokers to expand their offerings and better serve their clientele, enhancing both reach and revenue potential.',
+                        opacity:1,
+                    },'<')
+                    .set('.image-holder>img',{attr:{src:"./assets/prods-hand.png"}})
+                    .to('.image-holder',{
+                        left:'0',
+                        duration:0.2,
+                        ease:'power1'
+                    },'<')
+                    console.log('4');
+                    e.target.classList.add('active');
+                    break;
+                    case '5':
+                    
+                    tl.to(prodsHead,{
+                        textContent:' ',
+                        width:0,
+                        opacity:0,
+                        duration:.1,
+                        ease:'power1'
+                    })
+                    .to(prodsPara,{
+                        textContent:' ',
+                        opacity:0,
+                        duration:0.1
+                    },'<')
+                    .to('.image-holder',{
+                        left:'-150%',
+                        duration:0.2,
+                        ease:'power1'
+                    },'<')
+                    .to(prodsHead,{
+                        opacity:1,
+                    })
+                    
+                    .to(prodsHead,{
+                        width:'auto',
+                        // duration:.3,
+                        // ease:'power1'
+                    })
+                    .to(prodsHead,{
+                        textContent:'Cost Optimization Tools:',
+                    })
+                    .to(prodsPara,{
+                        textContent:"WINQ is designed to reduce brokerages' operational costs through automated workflows, simplified compliance procedures, efficient quote management, and more. No more exhaustive paperwork or excel sheets. Make life simple with WINQ.",
+                        opacity:1,
+                    },'<')
+                    .set('.image-holder>img',{attr:{src:"./assets/prods-hand.png"}})
+                    .to('.image-holder',{
+                        left:'0',
+                        duration:0.2,
+                        ease:'power1'
+                    },'<')
+                    console.log('5');
+                    e.target.classList.add('active');
+                    break;
+                    case '6':
+                    
+                    tl.to(prodsHead,{
+                        textContent:' ',
+                        width:0,
+                        opacity:0,
+                        duration:.1,
+                        ease:'power1'
+                    })
+                    .to(prodsPara,{
+                        textContent:' ',
+                        opacity:0,
+                        duration:0.1
+                    },'<')
+                    .to('.image-holder',{
+                        left:'-150%',
+                        duration:0.2,
+                        ease:'power1'
+                    },'<')
+                    .to(prodsHead,{
+                        opacity:1,
+                    })
+                    
+                    .to(prodsHead,{
+                        width:'auto',
+                        // duration:.3,
+                        // ease:'power1'
+                    })
+                    .to(prodsHead,{
+                        textContent:'Continuous Learning & Development:',
+                    })
+                    .to(prodsPara,{
+                        textContent:"WINQ provides access to comprehensive training modules to keep brokers updated on industry trends and equip them with the latest knowledge. (https://www.webce.com/)",
+                        opacity:1,
+                    },'<')
+                    .set('.image-holder>img',{attr:{src:"./assets/prods-hand.png"}})
+                    .to('.image-holder',{
+                        left:'0',
+                        duration:0.2,
+                        ease:'power1'
+                    },'<')
+                    console.log('6');
+                    e.target.classList.add('active');
+                    break;
+                    case '7':
+                    
+                    tl.to(prodsHead,{
+                        textContent:' ',
+                        width:0,
+                        opacity:0,
+                        duration:.1,
+                        ease:'power1'
+                    })
+                    .to(prodsPara,{
+                        textContent:' ',
+                        opacity:0,
+                        duration:0.1
+                    },'<')
+                    .to('.image-holder',{
+                        left:'-150%',
+                        duration:0.2,
+                        ease:'power1'
+                    },'<')
+                    .to(prodsHead,{
+                        opacity:1,
+                    })
+                    
+                    .to(prodsHead,{
+                        width:'auto',
+                        // duration:.3,
+                        // ease:'power1'
+                    })
+                    .to(prodsHead,{
+                        textContent:'Special Discount for WINQ Users',
+                    })
+                    .to(prodsPara,{
+                        textContent:'Unlock exclusive savings for WINQ users! Enjoy 10% to 15% discounts* on select items just for you. *Conditions apply ',
+                        opacity:1,
+                    },'<')
+                    .set('.image-holder>img',{attr:{src:"./assets/prods-hand.png"}})
+                    .to('.image-holder',{
+                        left:'0',
+                        duration:0.2,
+                        ease:'power1'
+                    },'<')
+                    console.log('7');
+                    e.target.classList.add('active');
+                    break;
+                    case '8':
+                    
+                    tl.to(prodsHead,{
+                        textContent:' ',
+                        width:0,
+                        opacity:0,
+                        duration:.1,
+                        ease:'power1'
+                    })
+                    .to(prodsPara,{
+                        textContent:' ',
+                        opacity:0,
+                        duration:0.1
+                    },'<')
+                    .to('.image-holder',{
+                        left:'-150%',
+                        duration:0.2,
+                        ease:'power1'
+                    },'<')
+                    .to(prodsHead,{
+                        opacity:1,
+                    })
+                    
+                    .to(prodsHead,{
+                        width:'auto',
+                        // duration:.3,
+                        // ease:'power1'
+                    })
+                    .to(prodsHead,{
+                        textContent:'Dedicated Quote Support:',
+                    })
+                    .to(prodsPara,{
+                        textContent:'WINQ facilitates seamless communication between brokers/brokerages and customers through a user-friendly live chat feature, ensuring prompt interaction and a faster sales cycle.',
+                        opacity:1,
+                    },'<')
+                    .set('.image-holder>img',{attr:{src:"./assets/prods-hand.png"}})
+                    .to('.image-holder',{
+                        left:'0',
+                        duration:0.2,
+                        ease:'power1'
+                    },'<')
+                    console.log('8');
+                    e.target.classList.add('active');
+                    break;
+                default:
+                    break;
+            }
+        })
+    })
+
+
